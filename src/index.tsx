9f432c4042f2b24534b2801c9407bc644609d6fe
JOSE CARLOS ALARCON MURILLO <josecarlosalarconmurillo@MacBook-Pro-de-JOSE.local>
2023-07-30 16:44:23 +0200

    boilerplate ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />);
